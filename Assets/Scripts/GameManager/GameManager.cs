9e7c116a256c97694f8199014bc62ad6b734f8eb
Jeroen <jeroen@vandegeest.eu>
2024-10-09 12:05:34 +0200

    Create GameManager

---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using JUtils;
+
+public class GameManager : StateMachine
+{
+    protected override void OnActivate()
+    {
+    }
+
+    protected override void OnDeactivate()
+    {
+    }
+
+    protected override void OnNoState()
+    {
+    }
+}
