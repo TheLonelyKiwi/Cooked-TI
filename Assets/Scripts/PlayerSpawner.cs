7dd8b08e710746720a65f7b0a353573da82bd3cd
TheLonelyKiwi <gamingkiwi22@gmail.com>
2024-10-10 19:51:32 +0200

    whooo randomness in games

---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    private List<Transform> _spawnPoints;
+    private List<Player> _players;
+    
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            _spawnPoints.Add(child);
+        }
+
+        _players = PlayerManager.instance.players;
+        
+        SpawnPlayers();
+    }
+
+    private void SpawnPlayers()
+    {
+        for (int i =0; i < _players.Count; i++)
+        {
+            int randomIndex = Random.Range(0, _spawnPoints.Count);
+            _players[i].gameObject.transform.position = _spawnPoints[randomIndex].position;
+            _spawnPoints.Remove(_spawnPoints[randomIndex]);
+        }
+        Debug.Log("Succesfully spawned players!");
+    }
+}
