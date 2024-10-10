7627029d918ba00c8e72813494e5feb9e340179c
TheLonelyKiwi <gamingkiwi22@gmail.com>
2024-10-10 16:55:07 +0200

    boot menu ui fixes

--- Assets/Scripts/GameManager/GamePlayState.cs
+++ Assets/Scripts/GameManager/GamePlayState.cs
@@ -7,6 +7,7 @@ public class GamePlayState : State
     protected override void OnActivate()
     {
         SceneManager.LoadScene("Game");
+        EventManager.OnTimerStart();
         foreach (Player player in PlayerManager.instance.players) {
             player.stateMachine.GoToState<DefaultPlayerState>();
         }
--- Assets/Scripts/UI/logicScript.cs
+++ Assets/Scripts/UI/logicScript.cs
@@ -1,20 +1,27 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class logicScript : MonoBehaviour
-    {
-    public int playerScore;
+{
+    public int playerScore = 0;
     public int scorePerObject = 10; 
-    public TMP_Text scoreText; 
+    public TMP_Text scoreText;
+    
+    private string _defaultText;
+
+    private void Start()
+    {
+        _defaultText = scoreText.text;
+        scoreText.text = $"{_defaultText} {playerScore}";
+    }
 
     [ContextMenu("Increase Score")]
-    public void addScore ()
-    {playerScore = playerScore + scorePerObject;
-    scoreText.text = playerScore.ToString();
+    public void addScore()
+    {
+        playerScore += scorePerObject;
+        scoreText.text = $"{_defaultText} {playerScore}";
     }
-
-    
-
-    }
+}
 
